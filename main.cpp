3e2035acabd38eb1da78ea595e624e2e7523f659
nopplz <79688748+nopplz@users.noreply.github.com>
2023-10-26 17:19:54 +0200

    Update main.cpp
    Code qui ne marche pas encore pour compter les battements du cœur.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
 
 int capteur =0;
+int led = 12;
 int sortie;
+int seuil;
+int compteur;
+int BPM;
+
+int comptage(int a, int b){
+  b=12 * a;
+  Serial.println(b);
+  a=0;
+} 
 
 void setup(){
-
+  pinMode(12,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop(){
   sortie =analogRead(capteur);
-  printf(sortie);
+  if (sortie>seuil){
+    digitalWrite(12,LOW); 
+  }
+  else{
+    digitalWrite(12,HIGH);
+    compteur++;
+  }
+  comptage(compteur,BPM);
   delay(1000);
 }
